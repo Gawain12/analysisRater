6ec0e3faf16fc55333c515f5622ac38d94b9a4a0
Gawain12 <tankaiyuan33@gmail.com>
2020-06-08 01:42:44 +0800

    更新正则解析算法，可以添加emoji入数据库，处理没有打分和评价的特殊条目，引入Redis作简单的缓存进度条和昵称的工具

--- Spider/Movie3.py
+++ Spider/Movie3.py
@@ -1,17 +1,24 @@
 import asyncio
 import re
-
+import sys
+import requests
 from fake_useragent import UserAgent
 import aiohttp
 import pandas as pd
+import xlwt
 import time
+import redis
 
 from Douban.Analysis.WeightScore import *
 from Douban.Database.myDb import connection_to_mysql
 #from haipproxy.client.py_cli import ProxyFetcher
 import Douban.Database.myDb
+pool = redis.ConnectionPool(host='localhost', port=6379,decode_responses=True)
+conn = redis.Redis(connection_pool=pool)
 
 
+global dfe
+dfe= pd.DataFrame()
 args = dict(host='127.0.0.1', port=6379, password=None, db=0)
 #fetcher = ProxyFetcher('douban', strategy='greedy', redis_args=args)
 
@@ -27,8 +34,12 @@ def get_random_proxy():
     return requests.get(proxypsslUrl).text.strip()
 '''
 class DataTool(object):
+    def __init__(self):
+        self.headers = {
+            'User-Agent': ua.random,
+        }
 
-    def newTupleData(self,originTupleData,rate,comments):
+    def newTupleData(self,originTupleData,rates,comments):
         #print(rate*2)
         newDict = {}
         p1 = re.compile(r'<li>', re.S)
@@ -46,21 +57,27 @@ class DataTool(object):
         p9 = re.compile(r'<spanclass="tags">', re.S)
         p10 = re.compile(r'&#39;',re.S)
         p11 = re.compile(r'&#34;',re.S)
+        p12 = re.compile(r'<span class="date">.*?</span>',re.S)
+        p13 = re.compile(r'<spanclass="pl">',re.S)
 
         name = re.sub(p10, "'", originTupleData[0])
         newDict['名称'] = name
 
         newDict['电影评分'] =float(originTupleData[3])
-        newDict['个人评分'] = rate*2
 
         newDict['评价人数'] =float(originTupleData[4])
         newDict['导演'] = originTupleData[1]
+
+        if rates == 'date':
+            rate = 0
+        else:
+            rate = int(re.findall(r'\d+', rates)[0]) * 2
 
         gut = re.sub(p3, '', originTupleData[2])
         gut = re.sub(p4, '', gut)
         newDict['类型'] = gut
 
-        comment = re.sub(p3, '', comments)
+        comment = re.sub(p12, '', comments)
         comment = re.sub(p4, '', comment)
         comment = re.sub(p5, '', comment)
         comment = re.sub(p6, '', comment)
@@ -69,32 +86,49 @@ class DataTool(object):
         comment = re.sub(p7, '', comment)
         comment = re.sub(p8, '', comment)
         comment = re.sub(p9, '', comment)
-        comment = re.sub(p10, '', comment)
-        comment = re.sub(p11, '', comment)
+        comment = re.sub(p10, "'", comment)
+        comment = re.sub(p11, "'", comment)
+        comment = re.sub(p3, '', comment)
+        comment = re.sub(p13, '', comment)
 
         #print(comment)
-        newDict['个人评价'] = comment
+        if comment=='':
+            newDict['个人评价']='还未评价'
+        else:
+            newDict['个人评价'] = comment
 
+        newDict['个人评分'] = rate
         return newDict
 
-class DataFrame(object):
+class Data(object):
 
     # 读取数据
     def __init__(self):
-       
+        # 创建工作簿
+        #self.cur, self.conn = mySql.CtM()
+
         self.engine, self.pymysql_session = connection_to_mysql()
+        self.writer = pd.ExcelWriter('E:\\Desktop\\test.xlsx')
        # 写入数据
     def writeData(self,row,movieDataDict,name):
         # ----------处理电影信息------------
         # 由字典转化成列表
+        global dfe
         dr = pd.DataFrame(movieDataDict,index = [0])
         eachDetail = []
         for value in movieDataDict.values():
             eachDetail.append(value)
-      
+
+        io = "../web/{}的片单.xlsx".format(name)
+        eachDetail = eachDetail[0:-1]
+        #del dr['_id']
         df = dr.astype(object).where(pd.notnull(dr), None)
         df.rename(columns={'名称': 'Name', '电影评分': 'Rate', '个人评分': 'MyRate', '评价人数': 'Num', '导演': 'Director', '类型': 'Type', '个人评价': 'MyComment'}, inplace=True)
         #print(df)
+        dfe=dfe.append(df,ignore_index=True,sort=False)
+        #print(dfe)
+        dfe.to_excel(io, sheet_name="mylist", na_rep="NoComment", index=False)
+        #df.to_csv('E:\\Desktop\\my_csv.csv', mode='a', header=False,encoding="GB18030")
         df.to_sql(name, self.engine, index=False, dtype=None, if_exists='append')
 
 
@@ -102,14 +136,39 @@ class DouBan(object):
     def __init__(self):
         self.headers = {
             'User-Agent' : ua.random,
-            'Cookie':'ll="118237"; bid=miJfnEe9C8E; __yadk_uid=C8tCYDQmeHxAfZlS5hvnGCCLtrU72prh; douban-fav-remind=1; ps=y; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1547354707%2C%22https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2Fqrconnect%3Fappid%3Dwxd9c1c6bbd5d59980%26redirect_uri%3Dhttps%253A%252F%252Fwww.douban.com%252Faccounts%252Fconnect%252Fwechat%252Fcallback%26response_type%3Dcode%26scope%3Dsnsapi_login%26state%3DmiJfnEe9C8E%252523douban-web%252523https%25253A%252F%252Fmovie.douban.com%252Ftop250%25253Fstart%25253D125%252526filter%25253D%22%5D; _pk_id.100001.4cf6=4179da9d4af6318e.1543568427.5.1547358498.1547349967.; _vwo_uuid_v2=D4FCFEDCB33DF07B2DE0DC525D42F7488|9024e242bbd9c0f98192b002e4091fc3; as="https://sec.douban.com/b?r=https%3A%2F%2Fmovie.douban.com%2Fsubject%2F26430107%2F"; dbcl2="189997368:Qj2bbphj+X0"; ck=iccT; push_noty_num=0; push_doumail_num=0'
-        }
+             }
         # 设置代理ip
         #self.proxy_list = {
              #"http": "http://202.121.96.33:8086",
          #}
-        self.base_pageUrl = 'https://movie.douban.com/people/{}/collect?start={}'
+        self.basePageUrl = 'https://movie.douban.com/people/{}/collect?start={}'
         #self.proxies=fetcher.get_proxy()
+    def Num(self,na,name):
+        pageUrl = self.basePageUrl.format(name, 0)
+        try:
+            response = requests.get(pageUrl, headers=self.headers)
+            if response.status_code == 200:
+                pageHtml = response.text
+                pattern = re.compile(
+                    r'<div id="db-usr-profile">.*?<div class="info">.*?<h1>(.*?)看过的电影(.*?)</h1>.*?</div>',
+                    re.S)
+                na = re.findall(pattern, pageHtml)
+                print(na)
+                if na:
+                 nick = na[0][0]
+                 num=int(na[0][1].strip('()'))
+
+                 print('nickName is {}'.format(nick))
+                 return nick,num
+                else:
+                    return '没有',3
+            else:
+                print('请求错误：url = {}, status_code = {}'.format(pageUrl, response.status_code))
+                sys.exit(0)
+                return None
+        except Exception as e:
+            print('请求异常：url = {}, error = {}'.format(pageUrl, e))
+            return None
     async def startCrawl(self,sem,session,num,name):
         # 使用代理ip
        # proxy = self.proxy_list
@@ -120,7 +179,7 @@ class DouBan(object):
         try:
             print('This is %s doubanlist' %name)
             print("Crawing %s" % num)
-            pageUrl = self.base_pageUrl.format(name,num)
+            pageUrl = self.basePageUrl.format(name,num)
             async with await session.get(pageUrl, headers=self.headers) as response:
                     if response.status == 200:
                         print('请求成功：url = {}, status = {}'.format(pageUrl, response.status))
@@ -129,13 +188,16 @@ class DouBan(object):
                         return responseText
                     else:
                         print('请求错误：url = {}, status = {}'.format(pageUrl, response.status))
+                        sys.exit(0)
                         return None
         except Exception as e:
             print('请求异常：url = {}, error = {}'.format(pageUrl, e))
-    # 获取详首页电影的 top_no movieUrl,返回一个列表
+    # 获取详首页电影的movieUrl rate comment,返回一个列表
+
+
     async def myList(self,pageHtml,urlsLists):
         # 书写正则表达式
-         pattern = re.compile(r'<div class="pic">.*?<a title="(.*?)" href="(.*?)" .*?>.*?<div class="info">.*?<li>.*?class="rating(.*?)-t">.*?class="date">.*?</span>.*?(.*?)</div>', re.S)
+         pattern = re.compile(r'<div class="pic">.*?<a title="(.*?)" href="(.*?)" .*?>.*?<div class="info">.*?<li>.*?class="(.*?)">.*?</span>.*?(.*?)</div>', re.S)
          movieInfoList = re.findall(pattern,pageHtml)
 
          urlsLists.extend(movieInfoList)
@@ -153,6 +215,7 @@ class DouBan(object):
                     return responseText
                 else:
                     print('请求错误：url = {}, status = {}'.format(movieUrl, response.status))
+
                     return None
       except Exception as e:
                 print('请求异常：url = {}, error = {}'.format(movieUrl, e))
@@ -160,28 +223,30 @@ class DouBan(object):
     async def movieDetails(self,movieHtml,row,rate,comment,name):
 
         pattern = re.compile(r'<div id="content">.*?<h1>.*?>(.*?)</span>.*? rel="v:directedBy">(.*?)</a>.*?<span class="pl".*?</span>(.*?)<br/>.*?<span class="pl">制片国家.*?<strong class="ll rating_num" property="v:average">(.*?)</strong>.*?<span property="v:votes">(.*?)</span>',re.S)
-        movieData = re.findall(pattern,str(movieHtml))
+        movie_data = re.findall(pattern,str(movieHtml))
         # 测试输出
-        print(movieData)
-        for movieTuple in movieData:
+        print(movie_data)
+        for movieTuple in movie_data:
             newData = DataTool().newTupleData(movieTuple, rate, comment)
+            Data().writeData(row,newData,name)
             row += 1
+            conn.incr('%s progress'%name)
             print(newData)
-        if movieData is None:
+        if movie_data is None:
             return None
-        return movieData
         # 保存到数据库中
+    async def download(self,sem,num,urlsLists,name):
+        async with aiohttp.ClientSession() as session:
+                html = await DouBan().startCrawl(sem,session,num,name)
+                await DouBan().myList(html,urlsLists)
     async def handle(self,sem,url,row,rate,comment,name):
         async with aiohttp.ClientSession() as session:
                 html = await DouBan().myHtml(sem,session,url)
                 await DouBan().movieDetails(html,row,rate,comment,name)
 
-    async def download(self,sem,num,urlsLists,name):
-        async with aiohttp.ClientSession() as session:
-                html = await DouBan().startCrawl(sem,session,num,name)
-                await DouBan().myList(html,urlsLists)
-def main(name):
 
+def main(name):
+    conn.set('%s progress'%name, '0')
     start = time.time()
     new_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(new_loop)
@@ -192,7 +257,10 @@ def main(name):
     sem = asyncio.Semaphore(100)
     urlsLists = []
     tasks = []
-    for i in range(0, 10):
+    na=[]
+    nick,num=dbObj.Num(na,name)
+    conn.rpush('{}'.format(name), nick, num)
+    for i in range(0, 6):
         if i % 15 == 0:
             task = asyncio.ensure_future(dbObj.download(sem, i, urlsLists, name))
             tasks.append(task)
@@ -201,7 +269,7 @@ def main(name):
 
     taskList = []
     for eachUrl in urlsLists:
-        rate = int(eachUrl[2])
+        rate = eachUrl[2]
         comment = eachUrl[3]
         task = asyncio.ensure_future(dbObj.handle(sem, eachUrl[1], row, rate, comment,name))
         taskList.append(task)
@@ -214,7 +282,6 @@ def main(name):
     db_session.commit()
     end = time.time()
     print('Cost time:', end - start)
-
 '''Name=web.app.req().name
-print(Name)'''
-main('gawain')
+print(Name)
+main('lemon0406')'''
--- web/app.py
+++ web/app.py
@@ -1,24 +1,27 @@
+import os
 import re
+import time
 
 import jieba
-from flask import Flask, render_template, url_for, request,session,redirect,g
+from flask import Flask, render_template, url_for, request, session, redirect, g, make_response, send_from_directory, \
+    Response
 import json
 import pymysql
 from zhon.hanzi import punctuation
 
-from Douban.Spider import Movie3
+
+from Douban.Spider.Movie3 import *
 from Douban.Analysis import DataProcessing
 from Douban.Database.myDb import *
-#import Spider.Movie3
 app = Flask(__name__)
 app.secret_key = '!@#$%^&*()11'
 app.debug = True
+import redis
 
+pool = redis.ConnectionPool(host='localhost', port=6379)
+conn = redis.Redis(connection_pool=pool)
 
 engine, db_session = connection_to_mysql()
-connection = pymysql.connect(host='localhost',user='root',passwd='password',db='douban',port=3306,charset='utf8')
-cur=connection.cursor()
-cur1=connection.cursor()
 
 '''@app.route('/ss', methods=['GET', 'POST'])
 def hello_world():
@@ -49,9 +52,12 @@ def req():
         name = session.get('user')
         print(name)
         print(session.get("password"))
-        Movie3.main(name)
+        main(name)
+        na=conn.lrange('{}'.format(name),0,1)
+        num=int(na[1])
+        nick=na[0]
         print(userNum)
-        return render_template('index.html',userNum=userNum,name=name)
+        return render_template('index.html',userNum=userNum,name=name,num=num,nick=nick)
     else:
         return render_template('index.html',userNum=userNum)
 
@@ -59,26 +65,47 @@ def req():
 def index():
     return render_template('index.html')
 
+@app.route("/download/<filename>", methods=['GET'])
+def download_file(filename):
+    # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)
+    directory = os.getcwd()  # 假设在当前目录
+    response = make_response(send_from_directory(directory, filename, as_attachment=True))
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode().decode('latin-1'))
+    return response
+
+@app.route('/progress/<string:name>/')
+def progress(name):
+    def generate():
+        x=1
+        while x < 15:
+            #Num = db_session.execute("select Count(*) from {}".format('name')).fetchall()
+            #x = int(conn.get('task'))
+            print('Progress of %s'%name)
+            x = int(conn.get('%s progress'%name))
+            time.sleep(0.1)
+            print('web %s' % x)
+            yield "data:" + str(x) + "\n\n"
+    return Response(generate(), mimetype='text/event-stream')
+
+
 @app.route('/chart/<string:name>/',methods=['POST','GET'])
 #链接数据库
 def chart(name):
-    print(name)
-    cur.execute("select * from user where Name = '" + name + "'")
-    cur1.execute("select MyComment from {}".format(name))
-    data = cur1.fetchall()
+    print('Its %s Chart' %name)
+    data = db_session.execute("select MyComment from {}".format(name)).fetchall()
     print(str(data))
-    jobNameli = []
+    wordCloud = []
     for d in data:
-        jobNameli.extend(jieba.lcut(re.sub("[{}]+".format(punctuation), "", d[0])))
+        wordCloud.extend(jieba.lcut(re.sub("[{}]+".format(punctuation), "", d[0])))
     dic = {}
-    for key in jobNameli:
+    for key in wordCloud:
         dic[key] = dic.get(key, 0) + 1
     print(dic)
     wordata = []
     for i in dic:
         wordata.append({"name": i, "value": dic[i], })
     print(wordata)
-    score = cur.fetchall()
+    score = db_session.execute("select * from user where Name = '" + name + "'").fetchall()
     print(score[0])
     data = DataProcessing.read_data(name)
     type, num = DataProcessing.type(data)
@@ -97,9 +124,6 @@ def chart(name):
     jsonData['cloud'] = wordata
     j = json.dumps(jsonData)
 
-    #依次把三个游标关闭
-    #cur.close()
-    #connection.close()
     return (j)
 
 if __name__ == '__main__':
